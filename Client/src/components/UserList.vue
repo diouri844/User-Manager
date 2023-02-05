f390a124e19fec3c00d3215a8f8fa6051a88d4e5
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2023-02-05 19:56:16 +0100

    update List user component

--- Client/src/components/UserList.vue
+++ Client/src/components/UserList.vue
@@ -14,72 +14,7 @@
                     <button class="btn btn-sm btn-outline-dark  me-2"><i class="bi bi-trash3"></i> Delete</button>
                     <button class="btn btn-sm btn-primary  me-2"><i class="bi bi-three-dots"></i> More</button>
                 </div>
-            </div>
-            <div class="card text-dark bg-light mb-3 col" style="max-width: 20rem;">
-                <div class="card-header">Header</div>
-                <div class="card-body">
-                    <h4 class="card-title">Secondary card title</h4>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                        content.</p>
-                </div>
-                <div class="card-footer text-white flex">
-                    <button class="btn btn-sm btn-light  me-2"><i class="bi bi-pen"></i> Update </button>
-                    <button class="btn btn-sm btn-outline-dark  me-2"><i class="bi bi-trash3"></i> Delete</button>
-                    <button class="btn btn-sm btn-primary  me-2"><i class="bi bi-three-dots"></i> More</button>
-                </div>
-            </div>
-            <div class="card text-dark bg-light mb-3 col" style="max-width: 20rem;">
-                <div class="card-header">Header</div>
-                <div class="card-body">
-                    <h4 class="card-title">Secondary card title</h4>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                        content.</p>
-                </div>
-                <div class="card-footer text-white flex">
-                    <button class="btn btn-sm btn-light  me-2"><i class="bi bi-pen"></i> Update </button>
-                    <button class="btn btn-sm btn-outline-dark  me-2"><i class="bi bi-trash3"></i> Delete</button>
-                    <button class="btn btn-sm btn-primary  me-2"><i class="bi bi-three-dots"></i> More</button>
-                </div>
-            </div>
-            <div class="card text-dark bg-light mb-3 col" style="max-width: 20rem;">
-                <div class="card-header">Header</div>
-                <div class="card-body">
-                    <h4 class="card-title">Secondary card title</h4>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                        content.</p>
-                </div>
-                <div class="card-footer text-white flex">
-                    <button class="btn btn-sm btn-light  me-2"><i class="bi bi-pen"></i> Update </button>
-                    <button class="btn btn-sm btn-outline-dark  me-2"><i class="bi bi-trash3"></i> Delete</button>
-                    <button class="btn btn-sm btn-primary  me-2"><i class="bi bi-three-dots"></i> More</button>
-                </div>
-            </div>
-            <div class="card text-dark bg-light mb-3 col" style="max-width: 20rem;">
-                <div class="card-header">Header</div>
-                <div class="card-body">
-                    <h4 class="card-title">Secondary card title</h4>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                        content.</p>
-                </div>
-                <div class="card-footer text-white flex">
-                    <button class="btn btn-sm btn-light  me-2"><i class="bi bi-pen"></i> Update </button>
-                    <button class="btn btn-sm btn-outline-dark  me-2"><i class="bi bi-trash3"></i> Delete</button>
-                    <button class="btn btn-sm btn-primary  me-2"><i class="bi bi-three-dots"></i> More</button>
-                </div>
-            </div>
-            <div class="card text-dark bg-light mb-3 col" style="max-width: 20rem;">
-                <div class="card-header">Header</div>
-                <div class="card-body">
-                    <h4 class="card-title">Secondary card title</h4>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                        content.</p>
-                </div>
-                <div class="card-footer text-white flex">
-                    <button class="btn btn-sm btn-light  me-2"><i class="bi bi-pen"></i> Update </button>
-                    <button class="btn btn-sm btn-outline-dark  me-2"><i class="bi bi-trash3"></i> Delete</button>
-                    <button class="btn btn-sm btn-primary  me-2"><i class="bi bi-three-dots"></i> More</button>
-                </div>
-            </div>
+            </div>    
         </div>
     </div>
 </template>
