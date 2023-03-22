e119afdd0ba1859cf2a20a94b6cc9670613afdfd
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2023-03-22 23:51:17 +0000

    Update Client entry point

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import { Center, Heading, Circle } from '@chakra-ui/react'
-import Login from './pages/Login'
+import { Center, Heading } from '@chakra-ui/react'
+import { Outlet } from 'react-router-dom'
 function App() {
 
   return (
-    <div className="App">
-      <Center bg='white' h='80px' color='black' variant="solid" mb={1}>
-        <Heading size={{ base: 'lg', md: 'xl' }}>User Manager </Heading> 
-      </Center>
-      <Login />
+    <div className="App"> 
+        <Center bg='white' h='80px' color='black' variant="solid" mb={1}>
+          <Outlet /> 
+        </Center>
     </div>
   )
 }
