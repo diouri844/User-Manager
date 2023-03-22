71e347814af524566980bf9f653d7898b870328f
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2023-03-22 23:55:00 +0000

    Setup  Validation

--- Server/Validations/ValidateUser.js
+++ Server/Validations/ValidateUser.js
@@ -3,8 +3,11 @@ const  User = require("../Models/User");
 
 
 
-const ValidateUserForm = (User)=>{
+const ValidateUserForm = (user_target)=>{
     // check user format :
+    console.table(
+        user_target
+    );
     return true;
 }
 
