43252332f1131171003167bd5f06e6e67f79212f
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2023-03-22 23:51:50 +0000

    Update Client main script

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,11 +4,27 @@ import App from './App'
 
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react'
+// import MyRouter provider 
+
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+
+import Login from './pages/Login';
+
+// define my router system : 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Login /> ,
+  },
+]);
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+      <ChakraProvider>
+        <RouterProvider router={router} />
+        <App />
+      </ChakraProvider>
   </React.StrictMode>
 )
