8f1c18fe612c1ee6a555aff89dd9280e4c345727
Chopen <75852177+diouri844@users.noreply.github.com>
2023-03-30 23:51:38 +0000

    Update CRUD User router

--- Server/Routes/User.js
+++ Server/Routes/User.js
@@ -3,7 +3,7 @@
 
 const Router = require('express').Router();
 const { User }  = require('../Models/User');
-const { GetUserByName } = require('../DB-Config/UserManager');
+const { GetUserByName, GetUserList } = require('../DB-Config/UserManager');
 const { GenerateHashedPassword } = require('../Helpers/PasswordManager');
 // define crud operations :
 
@@ -12,10 +12,6 @@ Router.get('/feeds',
     async ( req, res )=>{
         // get user name and password from request headre
         const { name, pswd } = req.headers;
-        console.log(
-            name,
-            pswd,
-        );
         // check if username and password is valid
         if(!name ||!pswd){
             res.status(401).json({
@@ -25,9 +21,6 @@ Router.get('/feeds',
 
         // get user from database
         const user = await GetUserByName(name)
-            console.log(
-                user
-            );
         if ( user === null ){
             res.status(402).json({
                 message: 'Invalid username or password'
@@ -40,24 +33,131 @@ Router.get('/feeds',
                 message: 'Invalid password'
             });
         }
-
+        // user exist and password is valid : 
+        // check if user connected or not : hase a token not equal to 0000:
+        if ( user.token === '0000' ){
+            res.status(401).json({
+                message: 'User is not connected'
+            });
+        }
+        // check user permissions : if role is admin :
+        if ( user.role != 'admin' ){
+            res.status(404).json({
+                message: 'Permission denied to access feeds',
+                operation: {
+                    name: 'get feeds',
+                    date: new Date().toJSON().slice(0,10).replace(/-/g,'/'),
+                    by: user._id
+                },
+                OperationBy: user
+            });
+        }
+        // all is greate now we sould get list of users :
+        // user exist and password is valid :
+        const response_user = await GetUserList();
         res.status(200).json(
             { 
-                users: [],
+                users: response_user || [],
                 message: 'Success',
                 operation: {
                     name: 'get feeds',
-                    date: new Date().toJSON().slice(0,10).replace(/-/g,'/')
+                    date: new Date().toJSON().slice(0,10).replace(/-/g,'/'),
+                    by: user._id
                 },
-                user: user
+                OperationBy: user
             }
         );
     }
-
-)
-
+);
 
 
+
+
+// router to create new feed <user> 
+
+
+Router.post('/feeds', 
+    async (req, res) => {
+        // get body from request :
+        const { Bodyname, Bodypswd, Bodyrole } = req.body;
+        // get token from header:
+        const { name, pswd } = req.headers;
+
+        if ( !Bodyname ||!Bodypswd ||!Bodyrole ){
+            res.status(401).json(
+                {
+                message: 'username, password and role required'
+                }
+            );
+        }
+        // check tokent usr is valid or not : 
+        const userToken = await GetUserByName(name);
+        if ( userToken === null ){
+            res.status(402).json(
+                {
+                message: 'Invalid Connexion token'
+                }
+            );
+        }
+        // check password hased is match or not : 
+        if ( userToken.password !== GenerateHashedPassword(pswd) ){
+            res.status(404).json(
+                {
+                    message: 'Invalid Password token'
+                }
+            );        
+        }
+        // chekc if user connected : 
+        if ( userToken.token!== '0000' ){
+            res.status(401).json(
+                {
+                    message: 'User is not connected'
+                }
+            );
+        }
+        // check user permissions : if role is admin :
+        if ( userToken.role!= 'Admin' ){
+            res.status(404).json(
+                {
+                    message: 'Permission denied to access feeds',
+                    operation: {
+                        name: 'create new feed',
+                        date: new Date().toJSON().slice(0,10).replace(/-/g,'/'),
+                        by: userToken._id
+                    },
+                    OperationBy: userToken
+                }
+            );
+        }
+        // all is greate now we sould get list of users :
+        // chec if user name is already used :
+        const user = await GetUserByName(name);
+        if ( user != null){
+            res.status(401).json(
+                {
+                message: 'username already used'
+                }
+            );
+        }
+        // create new user :
+        const new_user = await User.create({
+            name: Bodyname,
+            password: GenerateHashedPassword(Bodypswd),
+            role: Bodyrole,
+            token: '0000'
+        });
+        res.status(200).json(
+            {
+                message: 'Success',
+                operation: {
+                    name: 'create new feed',
+                    date: new Date().toJSON().slice(0,10).replace(/-/g,'/'),
+                    by: userToken._id
+                },
+                OperationBy: userToken
+            }
+        ); 
+    });
 
 
 
