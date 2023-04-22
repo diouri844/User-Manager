724cdc26ec116e1e4e1dfdac5330ae2e3f0a8c76
Chopen <75852177+diouri844@users.noreply.github.com>
2023-04-22 00:23:22 +0000

    Update Auth Router

--- Server/Routes/Auth.js
+++ Server/Routes/Auth.js
@@ -2,56 +2,12 @@ const User = require('../Models/User');
 
 const Router = require('express').Router();
 
-
-const { InserNewUser, ChechUser } = require('../DB-Config/UserManager');
-
-
+const { LoginController, RegisterController } = require('../Controllers/AuthController');
 
 // Login handler : 
-Router.post("/login",
-    async (req,res )=>{
-        // export name email password from req.body :
-        const { name, password } = req.body;
-        // trye to insert my user target : 
-        const {user,message,connected } = await ChechUser(
-            name,
-            password,
-        );
-        res.send(
-            {
-                user,
-                message,
-                connected
-            }
-        );
-    }
-);
-
-
+Router.post("/login",LoginController);
 
 // Registration Handler : 
-Router.post("/register",
-    async (req,res) =>{
-        // get body : 
-        const { name ,password, role } = req.body;
-        const response = await InserNewUser(
-            name,
-            password,
-            role
-        );
-        res.send(
-                {
-                    state:response.state,
-                    user:response.new_user,
-                    message:response.message,
-                    created:response.state === 200
-                }
-                );
-        }
-);
-
-
-
-
+Router.post("/register",RegisterController);
 
 module.exports = Router;
