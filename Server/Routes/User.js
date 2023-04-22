9310368c842acad86a072da1857c4393c3a8d41f
Chopen <75852177+diouri844@users.noreply.github.com>
2023-04-22 00:23:59 +0000

    Update Router

--- Server/Routes/User.js
+++ Server/Routes/User.js
@@ -3,15 +3,17 @@
 
 const Router = require('express').Router();
 const { getUsersController, getUserController , createUserController, UpdateUserController} = require("../Controllers/UserController");
+const CheckAuth = require("../middleware/Auth");
+
 // define crud operations :
 
-Router.get('/feeds',getUsersController);
+Router.get('/feeds',CheckAuth,getUsersController);
 // create new endpoint to get a feed by id 
-Router.get('/feeds/:id', getUserController);
+Router.get('/feeds/:id',CheckAuth,getUserController);
 // router to create new feed <user> 
-Router.post('/feeds',createUserController);
+Router.post('/feeds',CheckAuth,createUserController);
 // router to updae feed <user>: 
-Router.put('/feeds/:id', UpdateUserController);
+Router.put('/feeds/:id',CheckAuth,UpdateUserController);
 
 
 
