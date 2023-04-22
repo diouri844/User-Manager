0f7f99228d746d89ff9ef77ed891902f8ecf783a
Chopen <75852177+diouri844@users.noreply.github.com>
2023-04-22 00:24:49 +0000

    Add middleware

---
+++ Server/middleware/Auth.js
@@ -0,0 +1,42 @@
+// create a authentication chack middelware for user authentication
+const jwt = require('jsonwebtoken');
+const { User } = require("../Models/User");
+
+
+const CheckAuth = async (req, res, next ) => {
+    // get token from request header : 
+    try{
+        const token = req.headers.authorization.split(' ')[1];
+        if( !token ){
+            res.status(400).json({ message: "You are not authorized"});
+            return;
+        }
+        console.log( process.env.JWT_TOKEN_SECRET);
+        jwt.verify(
+            token, 
+            process.env.JWT_TOKEN_SECRET,
+            async (err, user_payload) => {
+                if ( err ){
+                    res.status(401).json({ message: err.message });
+                    return;
+                }
+                const user_target =  await User.findOne({_id: user_payload.id});
+                    console.table( user_target );
+                    req.user_token = user_target; 
+                    next();
+                    return;
+            });
+    }catch ( err ){
+        console.error( err.message );
+        res.status(500).json(
+            {
+                message:err.message
+            }
+        );
+    }
+
+}
+
+
+
+module.exports = CheckAuth;
