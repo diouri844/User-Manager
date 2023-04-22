b16ba1e8f4e9afdb2e46876bac831e78b956052f
Chopen <75852177+diouri844@users.noreply.github.com>
2023-04-22 00:24:22 +0000

    Create Auth controller

---
+++ Server/Controllers/AuthController.js
@@ -0,0 +1,43 @@
+const { InserNewUser, ChechUser } = require('../DB-Config/UserManager');
+
+const LoginController = async (req,res )=>{
+    // export name email password from req.body :
+    const { name, password } = req.body;
+    // trye to insert my user target : 
+    const {user,message,token ,connected } = await ChechUser(
+        name,
+        password,
+    );
+    res.send(
+        {
+            user,
+            message,
+            connected,
+            token
+        }
+    );
+}
+
+const RegisterController = async (req,res) =>{
+    // get body : 
+    const { name ,password, role } = req.body;
+    const response = await InserNewUser(
+        name,
+        password,
+        role
+    );
+    res.send(
+            {
+                state:response.state,
+                user:response.new_user,
+                message:response.message,
+                created:response.state === 200
+            }
+            );
+    }
+
+
+module.exports = { 
+    LoginController,
+    RegisterController
+};
