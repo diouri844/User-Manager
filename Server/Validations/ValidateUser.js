37c9644a13d096ba454cd8fc618e2cc900a650f1
Chopen <75852177+diouri844@users.noreply.github.com>
2023-05-03 00:45:47 +0000

    Manage user validate

--- Server/Validations/ValidateUser.js
+++ Server/Validations/ValidateUser.js
@@ -6,10 +6,40 @@ const  User = require("../Models/User");
 
 const ValidateUserForm = (user_target)=>{
     // check user format :
-    console.table(
-        user_target
-    );
-    return true;
+    if ( ! user_target.name || ! user_target.password ){
+        
+        return {
+            state : false ,
+            details: { 
+                name:"Name is required", 
+                password:"Password is required"
+            }
+        };
+    }
+
+    // check user email : 
+    if ( user_target.name.length === 0 
+        || user_target.email.includes(' ')){
+            return {
+                state : false ,
+                details: { 
+                    name:"Name not valid "
+                }
+            };
+        }
+    if ( user_target.password.length < 7 ||
+        user_target.password.includes(' ')){
+            return {
+                state : false ,
+                details: { 
+                    password:"Password not valid "
+                }
+            };
+        }
+    return {
+        state:true,
+        details:{}
+    }
 }
 
 
