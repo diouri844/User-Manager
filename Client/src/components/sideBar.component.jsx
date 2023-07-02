cedb66dd087535a51c91a86d3d89a994c095a036
Chopen <salahiddine.diouri@usmba.ac.ma>
2023-07-02 02:31:27 +0100

    Update sideabr component

--- Client/src/components/sideBar.component.jsx
+++ Client/src/components/sideBar.component.jsx
@@ -1,5 +1,7 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+
+
 import {
   IconButton,
   Box,
@@ -13,24 +15,25 @@ import {
   Text,
   useDisclosure,
 } from '@chakra-ui/react';
+
+
+
 import {
   FiHome,
-  FiTrendingUp,
   FiCompass,
   FiStar,
   FiSettings,
-  FiMenu,
   FiLogOut,
+  FiDatabase,
+  FiCheckCircle,
 } from 'react-icons/fi';
 
 
-const LinkItems = [
-  { name: 'Feeds', icon: FiHome , target:'/feeds'},
-  { name: 'Explore', icon: FiCompass , target:'/explore'},
-  { name: 'Favourites', icon: FiStar , target:'/favs'},
-  { name: 'Profile Settings', icon: FiSettings, target:'/settings' },
-  { name: 'Logout', icon:FiLogOut , target:'/logout'}
-];
+// import axios to handel request : 
+
+
+import axios from 'axios';
+
 
 export default function SimpleSidebar({ children }) {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -55,6 +58,66 @@ export default function SimpleSidebar({ children }) {
 
 
 const SidebarContent = ({ onClose, ...rest }) => {
+  const [isAdmin , setIsAdmin ] = useState(false);
+  // simple links for stuff user :
+  const LinkItems = [
+    { name: 'Feeds', icon: FiHome , target:'/feeds'},
+    { name: 'Explore', icon: FiCompass , target:'/explore'},
+    { name: 'Favourites', icon: FiStar , target:'/favs'},
+    { name: 'Profile Settings', icon: FiSettings, target:'/settings' },
+    { name: 'Logout', icon:FiLogOut , target:'/logout'}
+  ];
+  // create a dmin linkes : 
+  const AdminLinksItems = [
+    { name: 'Feeds', icon: FiHome , target:'/feeds'},
+    { name: 'Explore', icon: FiCompass , target:'/explore'},
+    { name: 'Favourites', icon: FiStar , target:'/favs'},
+    { name: 'Profile Settings', icon: FiSettings, target:'/settings' },
+    { name: 'Admin', icon:FiDatabase, target:'/admin'},    
+    {name: 'BanedUsers', icon:FiCheckCircle, target:'/baned'},
+    { name: 'Logout', icon:FiLogOut , target:'/logout'}
+  ];
+  useEffect( ()=>{
+    try{
+      // extract current userid from localstorage : 
+      const { AuthToken , UserId } = window.localStorage;
+      // check auth to extract the barrer :
+      var barrer = "Barer ";
+      if ( AuthToken[0] === '"' && AuthToken[AuthToken.length - 1] === "'") {
+          let updatedToken  = AuthToken.slice(1,-1);
+          barrer += updatedToken; 
+      }else{
+        barrer += AuthToken;
+      }
+      // config a request header : 
+      const config ={
+        'headers':{
+          'Authorization':barrer,
+          'Content-Type':'application/json',
+          'Accept':'*/*'
+        }
+      };
+      // check it the current user is an admin user :
+      axios.get(
+        `http://localhost:8080/api/manager/feeds/${UserId}`,
+        config
+      ).then( response => {
+        if ( response.data.message === 'Success'){
+          const userTarget = response.data.user;
+          if ( userTarget.role === 'Admin'){
+            setIsAdmin(true);
+          }
+        }
+      }).catch( error => { 
+        console.error('::)-> Can not fetch user : => \t'+error);
+       });
+    }catch( error ){
+      console.log( error );
+      setIsAdmin(false);
+    }
+  }, []);
+
+  
   return (
     <Box
       bg={useColorModeValue('white', 'gray.900')}
@@ -72,7 +135,7 @@ const SidebarContent = ({ onClose, ...rest }) => {
         </Text>
         <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
       </Flex>
-      {LinkItems.map((link) => (
+      {!isAdmin && LinkItems.map((link) => (
         <NavItem  
           key={link.name} icon={link.icon}
           >
@@ -84,6 +147,20 @@ const SidebarContent = ({ onClose, ...rest }) => {
             </NavLink>
         </NavItem>
       ))}
+      {/** conditional render a admin special links  */}
+      {isAdmin &&  AdminLinksItems.map(
+        ( adminLink )=> (
+          <NavItem key={adminLink.name} 
+          icon={adminLink.icon}
+          >
+            <NavLink 
+              key={adminLink.name}    
+              to={adminLink.target}>
+              { adminLink.name }
+            </NavLink>
+          </NavItem>
+        )
+      )}
     </Box>
   );
 };
