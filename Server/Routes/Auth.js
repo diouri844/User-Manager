64ac7348253819dfbb9642c72ce29afb000f885a
Chopen <salahiddine.diouri@usmba.ac.ma>
2023-07-03 23:48:39 +0100

    add check pswd endpoint to route

--- Server/Routes/Auth.js
+++ Server/Routes/Auth.js
@@ -1,13 +1,22 @@
 const User = require('../Models/User');
 
 const Router = require('express').Router();
+// add a check point for the user password 
 
-const { LoginController, RegisterController } = require('../Controllers/AuthController');
+
+const { LoginController, 
+        RegisterController, 
+        CheckPasswordController
+    } = require('../Controllers/AuthController');
 
 // Login handler : 
 Router.post("/login",LoginController);
 
 // Registration Handler : 
 Router.post("/register",RegisterController);
+
+// add the check  handler : 
+
+Router.post('/checkPassword/:userId',CheckPasswordController);
 
 module.exports = Router;
