b0102476b0bfe274997f15a269a0cd2706c005b9
Chopen <salahiddine.diouri@usmba.ac.ma>
2023-07-22 22:47:50 +0100

    Add Giga model

---
+++ Server/Models/Giga.js
@@ -0,0 +1,53 @@
+// import mongosse : 
+const mongoose = require('mongoose');
+
+
+// setup a giga schema 
+
+
+const GigaSchema = mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    area:{
+        type:String,
+        required:true
+    },
+    author_id:{
+        type: mongoose.Types.ObjectId,
+        required:true,
+        ref:'User'
+    },
+    body:{
+        type:String,
+        required:true
+    },
+    haseImg:{
+        type:Boolean,
+        required:true,
+        default:false
+    },
+    imgUrl:{
+        type:String
+    },
+    likes:{
+        type:Number,
+        default:0
+    },
+    shares:{
+        type:Number,
+        default:0
+    }
+});
+
+
+
+// create and export a model from the created schema : 
+
+const Giga = mongoose.model("Giga",GigaSchema);
+
+module.exports = {
+    Giga,
+    GigaSchema
+};
