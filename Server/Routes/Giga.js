31a020d65a2d1afd82cb88d6cf5f890362c09c40
Chopen <75852177+diouri844@users.noreply.github.com>
2023-07-27 15:55:56 +0200

    Update giga router

--- Server/Routes/Giga.js
+++ Server/Routes/Giga.js
@@ -4,12 +4,24 @@ const Router = require('express').Router();
 
 // import the controller handlers : 
 
-const { getGigsController, createGigsController } = require('../Controllers/GigaController');
+const { getGigsController, 
+        createGigsController,
+        getGigaAuthorInfoController,
+        getMyGigsController,
+        likeGigaController,
+        dislikeGigaController,
+        deleteGigsController
+    } = require('../Controllers/GigaController');
 const CheckAuth = require("../middleware/Auth");
 
 // setup the router :  
 Router.get('/Gigs',CheckAuth,getGigsController);
 Router.post('/Gigs', CheckAuth, createGigsController);
+Router.delete('/Gigs/:id',CheckAuth,deleteGigsController);
+Router.get('/Gigs/me',CheckAuth,getMyGigsController);
+Router.get('/Gigs/:id/Author',CheckAuth,getGigaAuthorInfoController);
+Router.get('/Gigs/:id/Like',CheckAuth,likeGigaController);
+Router.get('/Gigs/:id/Dislike',CheckAuth,dislikeGigaController);
 
 // export the router : 
 
