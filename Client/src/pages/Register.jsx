025be21110cbc6c284aba84e69d49d3103580f26
Chopen <salahiddine.diouri@usmba.ac.ma>
2024-07-23 23:01:06 +0200

    Update registration page

--- Client/src/pages/Register.jsx
+++ Client/src/pages/Register.jsx
@@ -15,13 +15,13 @@ import {
     AlertTitle,
     AlertDescription,
   } from '@chakra-ui/react'
-  
-  
-  
-  import { AtSignIcon, LockIcon, EmailIcon } from '@chakra-ui/icons';
-  import axios from "axios"; 
-  import { Link } from "react-router-dom";
 
+
+
+  import { AtSignIcon, LockIcon, EmailIcon } from '@chakra-ui/icons';
+  import axios from "axios";
+  import { Link } from "react-router-dom";
+  import { useToast } from '@chakra-ui/react'
   import { useRef, useState, useEffect } from 'react';
 
 
@@ -29,6 +29,8 @@ import {
     const [sending,setSending] =  useState(false);
     const [ErrorState, setErrorState] = useState(false);
     const [SuccessState, setSuccessState] = useState(false);
+    // time to setup toast :
+    const toaster = useToast();
     const Name = useRef("");
     const Password = useRef("");
     const Email = useRef("");
@@ -38,9 +40,9 @@ import {
 
 
 
-    async function HandelSubmit(){
-        console.log("HandelSubmit \n ");
-        setSending(true)
+    function HandelSubmit(){
+        //console.log("HandelSubmit \n ");
+        setSending(true);
         // extract the name , email and password;
         const emailtoSend = Email.current.value;
         const passwordtoSend = Password.current.value;
@@ -60,44 +62,56 @@ import {
         };
         console.table(
             payload
-        );   
-        // send the post request to the server 
-        const response = await axios.post(
+        );
+        // send the post request to the server
+        axios.post(
           "http://localhost:8080/api/users/register",
           payload,
           config
+        ).then(
+          response => {
+              setSending(false);
+              if ( response.data.created ){
+                setSuccessState(true);
+                // time to add my toaster :
+                toaster({
+                  title: 'Account created.',
+                  description: "We've created your account for you.",
+                  status: 'success',
+                  duration: 9000,
+                  isClosable: true,
+                })
+              }
+              else{
+                // error state :
+                toaster({
+                  title: 'oops',
+                  description: "Registration Failed",
+                  status: 'error',
+                  duration: 9000,
+                  isClosable: true,
+                })
+              }
+          }
+        ).catch(
+          error => {
+            setSending(false);
+            console.log("Error registration : ", error);
+            toaster({
+              title: 'oops',
+              description: "Somthing gose wrong with your registration",
+              status: 'error',
+              duration: 9000,
+              isClosable: true,
+            })
+          }
         );
-        setSending(false);
-        if ( response.data.created ){
-          setSuccessState(true);
-        }
-        console.log( response );
     }
 
     return(
         <Container maxW="lg" py={{ base: '12', md: '24' }} px={{ base: '0', sm: '8' }}>
-        {ErrorState && (
-            <Alert status='error' my={3}>
-              <AlertIcon />
-              <AlertTitle>Oooops !!</AlertTitle>
-              <AlertDescription>Your Email is not valid .</AlertDescription>
-          </Alert>
-          )}
-        {SuccessState && (
-            <Alert status='success' my={3}>
-              <AlertIcon />
-              <AlertTitle>{ UserState?.message }</AlertTitle>
-              {UserState?.connected && (
-                <AlertDescription>
-                  { UserState?.user?.name } 
-                </AlertDescription>  
-              )}
-              
-          </Alert>
-          )
-        }
         <Stack spacing="8">
-          <Heading size={{ base: 'sm', md: 'md' }} color='black'>  
+          <Heading size={{ base: 'sm', md: 'md' }} color='black'>
           Create your account
           </Heading>
         <Box
@@ -107,7 +121,7 @@ import {
           boxShadow={{ base: 'none', sm: 'md' }}
           borderRadius={{ base: 'none', sm: 'xl' }}
         >
-          
+
           <Stack spacing="6">
             <Stack spacing="5">
               <FormControl>
@@ -128,7 +142,7 @@ import {
                 <Input id="email" type="email" ref={Email} />
               </FormControl>
             </Stack>
-            
+
             <Stack spacing="5">
               <FormControl>
                 <FormLabel htmlFor="password">
@@ -137,10 +151,10 @@ import {
                 <Input id="password" type="password" ref={Password} />
               </FormControl>
             </Stack>
-            
+
             <HStack justify="space-between">
               <Link
-              variant="link" 
+              variant="link"
               tagName="Button"
               to="/"
               colorScheme="pink" size="sm"
@@ -152,7 +166,7 @@ import {
 
             <Stack spacing="6">
               <Button
-              onClick={()=> HandelSubmit() } 
+              onClick={()=> HandelSubmit() }
               colorScheme='blue' variant='solid'>
                   { sending && <Spinner color='white' /> }
                   { !sending && (<span>Sign up</span>) }
@@ -161,5 +175,5 @@ import {
           </Stack>
         </Box>
       </Stack>
-  </Container>  
+  </Container>
 )}
