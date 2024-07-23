b6902fd19a741060036577ddd3270afc3caac17a
Chopen <salahiddine.diouri@usmba.ac.ma>
2024-07-23 23:17:17 +0200

    Update component export point

--- Client/src/components/index.js
+++ Client/src/components/index.js
@@ -1,8 +1,8 @@
-export { default as SocialProfileWithImage } from "./profile.component";
+export { default as SocialProfileWithImage } from "./Profile/profile.component";
 export  { default as SimpleSidebar } from "./sideBar.component";
-export { default as Settings } from "./profileSettings.components";
-export { default as FeedList } from  "./feedList.component";
-export { default as FeedToolBar } from "./toolbare.component";
+export { default as Settings } from "./Profile/profileSettings.components";
+export { default as FeedList } from  "./Feeds/feedList.component";
+export { default as FeedToolBar } from "./Feeds/toolbare.component";
 export { default as Explore } from './explore.component';
-export { default as Logout } from './logout.component';
-export { default as Giga } from './giga.component';
+export { default as Logout } from './Profile/logout.component';
+export { default as Giga } from './Giga/giga.component';
